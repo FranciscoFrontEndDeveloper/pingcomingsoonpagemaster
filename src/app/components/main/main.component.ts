dc15b385c8c22eaf0937a0ef47833bab69a0f707
FRANCISCO <pachonetworking@gmail.com>
2023-06-23 16:42:00 -0500

    reactiveForm funtional

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.sass']
+  styleUrls: ['./main.component.sass'],
 })
 export class MainComponent {
+  formPing!: FormGroup;
+  formError!: boolean;
+  constructor(private formBuilder: FormBuilder) {
+    this.createform();
+    this.formError = false
+  }
 
+  createform() {
+    this.formPing = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required]],
+    });
+  }
+
+  submitForm() {
+    if (this.formPing.valid) {
+      this.formError = false;
+      console.log(this.formError)
+    } else {
+      this.formError = true;
+      console.log(this.formError)
+    }
+  }
 }
